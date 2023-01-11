dd645685dde7172e57e7739f73c4cd6fe56e8375
andersonvieirarj <andersonvieirarj@gmail.com>
2023-01-11 16:25:08 -0300

    Segunda versão

--- src/caixa/Checkout.java
+++ src/caixa/Checkout.java
@@ -1,4 +1,42 @@
 package caixa;
-
+import impressao.Impressora;
+import pagamento.Cartao;
+import pagamento.Operadora;
 public class Checkout {
+    // Finalizar a compra após ela ser autorizada pelo cartão
+    // Ela vai ter um método de fechar a compra, que vai receber uma
+    // compra e um cartão.
+    private Operadora operadora;
+    private Impressora impressora;
+    public Checkout(Operadora operadora, Impressora impressora) {
+        this.operadora = operadora;
+        this.impressora = impressora;
+    }
+    public Operadora getOperadora() {
+        return operadora;
+    }
+    public void setOperadora(Operadora operadora) {
+        this.operadora = operadora;
+    }
+    public Impressora getImpressora() {
+        return impressora;
+    }
+    public void setImpressora(Impressora impressora) {
+        this.impressora = impressora;
+    }
+    //Para fechar a compra, eu preciso receber uma compra e um cartão,
+    // além de verificar se ele está autorizado ou não
+    public void fecharCompra (Compra compra, Cartao cartao){
+        boolean autorizado = this.operadora.autorizar(compra, cartao);
+        if (autorizado){
+            this.impressora.imprimir(compra);
+        }
+        else {
+            System.out.println("Compra Não Autorizada");
+        }
+    }
+    //Se autorizado, eu posso imprimir um documento imprimível
+    // (alguém que segue o contrato!) Como compra tem implementação
+    // da interface Imprimível,eu tbm posso passar "compra"
+    // como documento imprimível
 }
--- src/pagamento/Principal.java
+++ src/pagamento/Principal.java
@@ -1,7 +1,27 @@
 package pagamento;
 
+import caixa.Checkout;
+import caixa.Compra;
+import impressao.HP;
+import impressao.Impressora;
+
 public class Principal {
     public static void main(String[] args) {
+        // Para testar: precisamos instanciar uma operadora, uma impressora,
+        // um cartão do cliente, uma compra, e finalmente instanciar um
+        // checkout
+        Operadora operadora = new Nubank();
+        Impressora impressora  = new HP();
+        Cartao cartao = new Cartao();
+        cartao.setNomeDoCliente("Anderson Vieira");
+        cartao.setNumeroCartao("1234.5555.5555.4434");
+        Compra compra = new Compra();
+        compra.setNomeCliente("Anderson Vieira");
+        compra.setProduto("Mouse Microsoft Bluetooth 5665");
+        compra.setValorCompra(199.99);
+        //Finalizando a compra do produto
+        Checkout checkout = new Checkout(operadora,impressora);
+        checkout.fecharCompra(compra,cartao);
 
     }
 }
